896f26f778f657c8cff92c0456de825d72a6ae27
ivatiuk <engs-fabricioivatiuk@camporeal.edu.br>
2026-05-31 16:10:41 -0300

    atualizaçao gravetos

--- jogo.cpp
+++ jogo.cpp
@@ -18,8 +18,13 @@ int main () {
     int metafrutas = 0;
     int metaanimais = 0;
     int metaarvores = 0;
-    char objetivo;
+    int objetivo;
     srand(time(0));
+    for(int i = 0; i < 16; i++){
+        for(int l = 0; l < 16; l++){
+            mapa[i][l] = 0;
+        }
+    }
 
     cout << "qual o seu objetivo ?";
     cout <<" 1- Coletar 4 gravetos e 2 pedras" << endl;
@@ -30,23 +35,25 @@ int main () {
     if(objetivo == '1') {
     metagravetos = 4 ;
     metapedras = 2 ;
+     cout <<" 1- Coletar 4 gravetos e 2 pedras" << endl;
     }
     else if(objetivo == '2') {
      metafrutas = 1;
      metaanimais = 3;
+     cout <<" 2- coletar 1 fruta e 3 animais" << endl; 
     }
     else if (objetivo == '3'){
         metapedras = 8;
-        metaarvores = 4;}
-    else {
-        cout << "objetivo invalido ";
+        metaarvores = 4;
+     cout << "3- coletar 8 pedras e 4 arvores" << endl;
+    }
+    for(int i = 0; i < 38; i++){
+        x = rand() % 16;
+
+        y = rand() % 16;
+        
+        mapa[x][y] = 1;
     }
 
-
-    for (int i = 0; i < 38 ; i++) {
-       //x= %16;
-     //  y = %16;
-       // mapa[x][y]= "g";
-
-    }
 }
+
